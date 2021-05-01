a7ff50e7804c873f62387d55effee36f124db006
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-01 13:15:43 -0500

    feat: getDouble(double min, double max) done

--- src/util/Input.java
+++ src/util/Input.java
@@ -36,6 +36,28 @@ public class Input {
         }while(true);
 return getInt;
     }
+    
+    public int getInt(){
+        return scanner.nextInt();
+    }
+
+    public double getDouble(double min, double max){
+        double getDouble;
+        do{
+            getDouble = scanner.nextDouble();
+            System.out.println("Enter a number between 1.5 and 20.67");
+            if(getDouble <1.5 || getDouble > 20.67){
+                System.out.println("Sorry that number is not in the range of numbers we asked for...");
+            }else if (getDouble > 1.5 && getDouble <20.67){
+                System.out.println("Thanks ! The number in range you entered is " + getDouble);
+                break;
+            }
+        }while(true);
+        return getDouble;
+    }
+    public double getDouble(){
+        return scanner.nextDouble();
+    }
 
     public static void main(String[] args) {
         Input input= new Input();
@@ -47,6 +69,14 @@ return getInt;
 
         System.out.println("Choose a number between 1 and 10 please.");
         System.out.println(input.getInt(1,10));
+
+        System.out.println("Now choose a number, any number !");
+        System.out.println("input.getInt() = " + input.getInt());
+
+        System.out.println("Pick a number with a decimal between 1.5 and 20.67");
+        System.out.println( input.getDouble(1.5,20.67));
+
+
     }
 
 
