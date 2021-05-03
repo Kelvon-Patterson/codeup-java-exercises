31837ffe1876764fd95f37562ddbceb56241fc1c
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-03 09:31:11 -0500

    feat : refactoring input.java and input.test

---
+++ src/shapes/Circle.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius, double radius1) {
+
+        this.radius = radius1;
+    }
+
+    public double getArea() {
+        return  radius * (Math.pow(Math.PI,2)) ;
+    }
+
+    public double getCircumference() {
+        return ;
+    }
+}
--- src/util/Input.java
+++ src/util/Input.java
@@ -59,26 +59,6 @@ return getInt;
         return scanner.nextDouble();
     }
 
-    public static void main(String[] args) {
-        Input input= new Input();
-        System.out.println("What is your name ?");
-        System.out.println("Your name is "+ input.getString() + "!");
-
-        System.out.println("Is true = true and false =false?");
-        System.out.println(input.yesNo());
-
-        System.out.println("Choose a number between 1 and 10 please.");
-        System.out.println(input.getInt(1,10));
-
-        System.out.println("Now choose a number, any number !");
-        System.out.println("input.getInt() = " + input.getInt());
-
-        System.out.println("Pick a number with a decimal between 1.5 and 20.67");
-        System.out.println( input.getDouble(1.5,20.67));
-
-        System.out.println("Pick any number with a decimal!!");
-        System.out.println("input.getDouble() = " + input.getDouble());
-    }
 
 
 }
