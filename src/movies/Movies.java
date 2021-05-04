8711c6a35be4881a1568c89502f2a6c8086b3bfa
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-04 11:52:39 -0500

    feat: Began exercise #3 Movies exercise

--- src/movies/Movies.java
+++ src/movies/Movies.java
@@ -1,10 +1,30 @@
 package movies;
 
-public class Movies {
+class Movie {
     private String name;
     private String category;
-
+    //Name Getter
     public String getName(){
+        return name;
+    }
+    //Name Setter
+    public void setName(String newName){
+        this.name = newName;
+    }
+    //Category Getter
+    public String getCategory(){
+        return category;
+    }
+    //Category Setter
+    public void setCategory(String newCategory){
+        this.category= newCategory;
+    }
+    public Movie(String movieName, String category){
+        name=movieName;
+        category="Unassigned";
+    }
+
+    public static void main(String[] args) {
 
     }
 }
