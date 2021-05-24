52dff078065cb34234d1667192eab31c2368bbe4
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-23 21:21:13 -0500

    feat: startOz method

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -238,7 +238,15 @@ public static String stringBits(String str) {
    // Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o' and include the second only if it is 'z', so "ozymandias" yields "oz".
 
     public static String startOz(String str){
-
+        if(str.substring(0,2).startsWith("oz")){
+            return str.substring(0,2);
+        }else if(str.substring(0,2).contains("z")){
+            return "z";
+        }else if (str.substring(0,2).contains("o")){
+            return "o";
+        }else{
+            return str;
+        }
     }
 
 
@@ -268,6 +276,9 @@ public static String stringBits(String str) {
         System.out.println(mixStart("mix snacks"));
         System.out.println(mixStart("pix snacks"));
         System.out.println(mixStart("piz snacks"));
+        System.out.println(startOz("ozymandias"));
+        System.out.println(startOz("bzoo"));
+        System.out.println(startOz("oxx"));
     }
 
 
