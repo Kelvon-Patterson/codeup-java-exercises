2c7bfd12f5415efc6deaf286b724f1c383e3fa89
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-05-24 08:58:54 -0500

    feat: closer10

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -263,6 +263,19 @@ public static String stringBits(String str) {
         }
     }
 
+    public static int closer10(int a, int b){
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+
+        if (aDiff < bDiff) {
+            return a;
+        }
+        if (bDiff < aDiff) {
+            return b;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("sumDouble() = " + sumDouble(9,9));
