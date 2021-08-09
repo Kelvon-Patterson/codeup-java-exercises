01c71c686ce4a5c0ed41a16006eb770732a2a76d
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-09 18:35:40 -0500

    feat: Java loops II

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -5,6 +5,27 @@ import java.util.Scanner;
 
 public class HackerRank {
     public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            for(int j=0; j<n; j++){
+                a+=b;
+
+                if (j>0){
+                    System.out.print(" ");
+                }
+                System.out.print(a);
+                b=b*2;
+            }
+            System.out.print("\n");
+        }
+        in.close();
+
+
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
 
