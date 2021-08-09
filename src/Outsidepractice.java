64323439643d36cf5095d8249f75377ef8cc6aab
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-09 17:11:49 -0500

    feat: java output formatting

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -313,6 +313,17 @@ public static String stringBits(String str) {
         scan.nextLine();
         String s = scan.nextLine();
 
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for( i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
