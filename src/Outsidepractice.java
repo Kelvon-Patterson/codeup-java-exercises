ca55bec0551d3f48c52205b7450a99b2b1d17a8a
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-09 17:18:42 -0500

    Feat: hacker rank solution problems

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -304,29 +304,7 @@ public static String stringBits(String str) {
     }
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
 
-        // Write your code here.
-
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
-
-        Scanner sc=new Scanner(System.in);
-        System.out.println("================================");
-        for( i=0;i<3;i++)
-        {
-            String s1=sc.next();
-            int x=sc.nextInt();
-            //Complete this line
-            System.out.printf("%-15s%03d%n", s1, x);
-        }
-        System.out.println("================================");
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
         System.out.println("sumDouble() = " + sumDouble(9,9));
         System.out.println("diff21() = " + diff21(100));
         System.out.println("parrotTrouble(true,7) = " + parrotTrouble(true, 7));
