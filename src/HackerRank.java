b5f2764a95d12e1620ffc1b987d905070f3dd1ce
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-10 13:54:15 -0500

    feat: imported java.util.locale for findDay method

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Calendar;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class HackerRank {
