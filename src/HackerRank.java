528b69a2e02738215bd92e2335dd9502f2288461
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-08-11 08:47:04 -0500

    feat: currency formatting problem

--- src/HackerRank.java
+++ src/HackerRank.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.NumberFormat;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.Scanner;
@@ -17,6 +18,8 @@ public class HackerRank {
         return dayOfWeek;
 
     }
+
+
 
 
     static int B;
@@ -44,6 +47,23 @@ public class HackerRank {
     String s = Integer.toString(n);
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        Locale indiaLocale = new Locale("en","IN");
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         for (int i = 0; i < t; i++) {
