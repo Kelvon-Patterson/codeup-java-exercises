9621252a6401d1cce7f83162a9aae47f315a36f2
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-10-02 23:47:07 -0500

    feat: original

--- src/W3resourcesExercises.java
+++ src/W3resourcesExercises.java
@@ -155,7 +155,15 @@ public class W3resourcesExercises {
                 ArrayList<String> newColor1 = (ArrayList<String>) colors1.clone();
                 System.out.println("Cloned colors1 array list= " + newColor1);
 
-
+                ArrayList<String> c1= new ArrayList<String>();
+                c1.add("Red");
+                c1.add("Green");
+                c1.add("Black");
+                c1.add("White");
+                c1.add("Pink");
+                System.out.println("Original array list: " + c1);
+                c1.removeAll(c1);
+                System.out.println("Array list after remove all elements "+c1);
 
 
 
