2082277e9fe91616243f446f2084fb66acebf1a3
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-10-16 14:28:44 -0500

    feat: biggerTwo

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -499,7 +499,17 @@ public int areaOfTriangle(int base, int height){
         }
         return count;
     }
-
+    public int[] biggerTwo(int[] a, int[] b) {
+        int aSum = a[0] + a[1];
+        int bSum = b[0] + b[1];
+        if(aSum > bSum ){
+            return a;
+        }else if(bSum > aSum){
+            return b;
+        }else {
+            return a;
+        }
+    }
     public static void main(String[] args) {
 
 
