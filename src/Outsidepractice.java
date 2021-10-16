22feb537a2c8864f8323f17484dc65533005c3d7
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-10-16 14:07:06 -0500

    feat: double23

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -472,6 +472,14 @@ public int areaOfTriangle(int base, int height){
         newArr[newArr.length - 1] = nums[nums.length - 1];
         return newArr;
     }
+    public boolean double23(int[] nums) {
+        if(nums.length == 2 && nums[0] ==2 && nums[1] == 2){
+            return true;
+        }else if( nums.length == 2 && nums[0] == 3 && nums[1] == 3){
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
 
