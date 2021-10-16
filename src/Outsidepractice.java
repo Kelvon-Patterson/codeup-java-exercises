1be1458ad16972b4fc4562743516848f8f1706e3
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-10-16 14:23:36 -0500

    feat: start1

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -489,6 +489,16 @@ public int areaOfTriangle(int base, int height){
         }
         return nums;
     }
+    public int start1(int[] a, int[] b) {
+        int count = 0;
+        if(a.length >=1 && a[0] == 1){
+            count++;
+        } if(b.length >=1 && b[0] == 1){
+            count++;
+
+        }
+        return count;
+    }
 
     public static void main(String[] args) {
 
