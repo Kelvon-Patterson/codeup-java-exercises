00904950c789771be424078494f221bc818b914c
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-10-16 14:12:05 -0500

    feat: fix23

--- src/Outsidepractice.java
+++ src/Outsidepractice.java
@@ -481,6 +481,15 @@ public int areaOfTriangle(int base, int height){
         return false;
     }
 
+    public int[] fix23(int[] nums) {
+        if(nums[0] == 2 && nums[1] == 3){
+            nums[1] = 0;
+        }else if(nums[1] == 2 && nums[2] == 3){
+            nums[2] = 0;
+        }
+        return nums;
+    }
+
     public static void main(String[] args) {
 
 
